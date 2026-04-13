678392ef4806ab07e2e99cd68642d2f463dc7967
YSakh <Yuriy.Sakhanovych@gmail.com>
2026-04-13 13:35:11 -0400

    Uses self-hosted runners

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ def details():
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, human!'
+        'message': 'You are doing great, human :)'
     })
 
 @app.route("/api/v1/healthz")
